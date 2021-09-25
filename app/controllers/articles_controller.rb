2a69a5528a134302e1cf9e94e2ec0928d62f1ba6
ricardo caceres <ricardocacerez@gmail.com>
2021-09-25 11:23:13 -0300

    creando vista y accioneas para actualizar articulos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
-  def index
+    def index
     @articles = Article.all
-  end
-
+    end
+  
   def show
     @articles = Article.find(params[:id]) 
   end
@@ -13,13 +13,27 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-
+ 
     if @article.save
       redirect_to @article
     else
       render :new
     end
-  end    
+  end  
+  
+  def edit 
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+      else
+      render :edit
+    end
+  end
 
   private
     def article_params
